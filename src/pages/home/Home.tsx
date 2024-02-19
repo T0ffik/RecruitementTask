b00b74dc289bd44f6861e6797905bad182a5b62b
T0ffik <t0fffik2000@gmail.com>
2024-02-19 16:49:01 +0100

    feat: add filter section

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,22 +1,11 @@
-import { Button } from "@mui/material";
-import { useSelector } from "@xstate/react";
-import { productsActor } from "../../xstate/productsActor";
 import { ProductsTable } from "../../components/Table";
+import { Filter } from "../../components/Filter";
+import "./styles.scss";
 
 export const Home = () => {
-  const state = useSelector(productsActor, (state) => state);
   return (
-    <div>
-      {JSON.stringify(state.value)}
-      <Button variant="contained" onClick={() => console.log("Hello to you!")}>
-        Hello world!
-      </Button>
-      <Button
-        variant="contained"
-        onClick={() => productsActor.send({ type: "Load" })}
-      >
-        Load
-      </Button>
+    <div className="AppWrapper">
+      <Filter />
       <ProductsTable />
     </div>
   );
