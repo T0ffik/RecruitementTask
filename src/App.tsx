3b3db29f5fdc936390225565fd76b73c69880612
T0ffik <t0fffik2000@gmail.com>
2024-02-19 14:15:30 +0100

    feat: add global state handling

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,8 @@
-import { Button } from "@mui/material";
 import "./App.css";
-import { useMachine } from "@xstate/react";
-import { productsMachine } from "./xstate/productsMachine";
+import { Home } from "./pages/home/Home";
 
 function App() {
-  const [state, send] = useMachine(productsMachine);
-  return (
-    <div>
-      {JSON.stringify(state.value)}
-      <Button variant="contained" onClick={() => console.log("Hello to you!")}>
-        Hello world!
-      </Button>
-      <Button variant="contained" onClick={() => send({ type: "Load" })}>
-        Load
-      </Button>
-    </div>
-  );
+  return <Home />;
 }
 
 export default App;
---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,21 @@
+import { Button } from "@mui/material";
+import { useSelector } from "@xstate/react";
+import { productsActor } from "../../xstate/productsActor";
+
+export const Home = () => {
+  const state = useSelector(productsActor, (state) => state);
+  return (
+    <div>
+      {JSON.stringify(state.value)}
+      <Button variant="contained" onClick={() => console.log("Hello to you!")}>
+        Hello world!
+      </Button>
+      <Button
+        variant="contained"
+        onClick={() => productsActor.send({ type: "Load" })}
+      >
+        Load
+      </Button>
+    </div>
+  );
+};
