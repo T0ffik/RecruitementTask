a80cb07c0c6b27ad936705bd04a8b7e58f9ec2e2
T0ffik <t0fffik2000@gmail.com>
2024-02-20 13:54:35 +0100

    fix: typescript errors

--- src/xstate/products/actions.ts
+++ src/xstate/products/actions.ts
@@ -1,10 +1,18 @@
+import { AssignArgs, assertEvent } from "xstate";
+import { ProductsState, TActor, TEvents } from "../../types/types";
 import { id, page } from "../../utils/consts";
 
-export const assignId = (action) => {
+export const assignId = (
+  action: AssignArgs<ProductsState, TEvents, TEvents, TActor>
+) => {
+  assertEvent(action.event, "Filter");
   window.history.replaceState("filter", "", `?${id}=${action.event.id}`);
   return action.event.id;
 };
-export const assignPage = (action) => {
+export const assignPage = (
+  action: AssignArgs<ProductsState, TEvents, TEvents, TActor>
+) => {
+  assertEvent(action.event, "ChangePage");
   window.history.replaceState(
     "pagination",
     "",
