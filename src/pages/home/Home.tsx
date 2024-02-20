de01f23bcc28510d08c3b02b4209d6aace5f3913
T0ffik <t0fffik2000@gmail.com>
2024-02-20 09:52:26 +0100

    feat: add error message

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,16 +1,10 @@
-import { ProductsTable } from "../../components/Table";
-import { Filter } from "../../components/Filter";
 import "./styles.scss";
-import { CustomPagination } from "../../components/Pagination";
-import { BasicModal } from "../../components/Modal";
+import { Content } from "../../components/PageContent";
 
 export const Home = () => {
   return (
     <div className="AppWrapper">
-      <BasicModal />
-      <Filter />
-      <ProductsTable />
-      <CustomPagination />
+      <Content />
     </div>
   );
 };
